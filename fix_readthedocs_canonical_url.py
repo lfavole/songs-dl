a4e53cc594fc1bd51acd98e6395119808c0b28a0
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-04-04 17:03:02 +0200

    Fix ReadTheDocs canonical URL

---
+++ fix_readthedocs_canonical_url.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+import re
+
+url = os.getenv("READTHEDOCS_CANONICAL_URL")
+mkdocs_yml = Path("mkdocs.yml")
+data = mkdocs_yml.read_text("utf-8")
+
+print(f"Replacing site_url by {url}")
+data = re.sub(r"(?m)^site_url: .*$", f"site_url: {url}", data)
+
+mkdocs_yml.write_text(data, "utf-8")
